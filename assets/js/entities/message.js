8b7bb50df4a9fad13b36623bda9a725df9d76482
David-Shi-1989 <David-Shi-1989@users.noreply.github.com>
2017-12-06 07:44:50 +0800

    index

--- assets/js/entities/message.js
+++ assets/js/entities/message.js
@@ -15,32 +15,37 @@ BlogAppManager.module("Entities",function (Entities,BlogAppManager,Backbone,Mari
                 nickname:"MM",
                 datetime:"2017/11/23 10:23:11",
                 content:"这是一个有声音的留言",
+                avatar:"assets/image/avatar/1.jpg"
             },
             {
                 id:2,
                 nickname:"M1231撒旦",
                 datetime:"2017/11/23 04:23:11",
                 content:"此处无声胜有声",
+                avatar:"assets/image/avatar/2.jpg",
                 items: [
                     {
                         id:4,
                         nickname:"M12旦",
                         datetime:"2017/11/23 04:23:11",
-                        content:"回复1"
+                        content:"回复1",
+                        avatar:"assets/image/avatar/3.jpg"
                     },
                     {
                         id:5,
                         nickname:"M12旦",
                         datetime:"2017/11/23 04:23:11",
-                        content:"回复2"
-                    },
+                        content:"回复2",
+                        avatar:"assets/image/avatar/4.jpg"
+                    }
                 ]
             },
             {
                 id:3,
                 nickname:"诗人",
                 datetime:"2017/11/21 22:23:11",
-                content:"欲把西湖比西子",
+                content:"那么这两个区别是什么呢？假设我们需要加一个表头，提示列名。原来的CollectionView实现不了，因为它只能指定标签名为table，不能加。这时，我们想要是也能给table设一个template多好。对了！CompositeView就能做到。这也是二者最大的区别。",
+                avatar:"assets/image/avatar/5.jpg",
                 items:[]
             }
         ]);
--- assets/js/list/message_list.js
+++ assets/js/list/message_list.js
@@ -1,26 +1,14 @@
 BlogAppManager.module("List",function (List,BlogAppManager,Backbone,Marionette,$,_){
     List.MessageListItem = Marionette.ItemView.extend({
-        tagName:"tr",
-        template:"#message-page-item",
-        events:{
-            "click div.image-item-imageWrapper":"openCurrentAlbum",
-            "click div.image-item-des > a":"openCurrentAlbum"
-        },
-        openCurrentAlbum:function (evt) {
-            if(evt){
-                evt.preventDefault();
-                evt.stopPropagation();
-            }
-            BlogAppManager.navigate("album/"+this.model.get("id"));
-            BlogAppManager.Controller.showAlbum.showAlbumById(this.model.get("id"));
-        }
+        tagName:"li",
+        template:"#message-page-item"
     });
 
     List.MessageListView = Marionette.CompositeView.extend({
-        tagName: "table",
-        id: "messageTable",
+        tagName: "div",
+        id: "messageListContainer",
         childView: List.MessageListItem,
         template: "#message-page",
-        childViewContainer:"tbody"
+        childViewContainer:"ul"
     });
 });
