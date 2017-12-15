3d76841c703b2fde52c0cc1a1d5ed82bbf8a8d80
David-Shi-1989 <David-Shi-1989@users.noreply.github.com>
2017-12-15 08:05:33 +0800

    index

--- assets/js/list/message_list.js
+++ assets/js/list/message_list.js
@@ -20,7 +20,11 @@ BlogAppManager.module("List",function (List,BlogAppManager,Backbone,Marionette,$
         switchEmojiClass:function (evt) {
             evt.preventDefault();
             evt.stopPropagation();
-            $(evt.target).add($(this.$el).find("div.msg-emoji-class span.emoji-hover")).toggleClass("emoji-hover").toggleClass("emoji-normal");
+            var newIndex = $(evt.target).parent().parent().index(),oldIndex = $(this.$el).find("div.msg-emoji-class span.emoji-hover").parent().parent().index();
+            if(newIndex != oldIndex){
+                $($(this.$el)).find("div.msg-emoji-list > div").animate({marginLeft:(newIndex*-600)+"px"},200);
+                $(evt.target).add($(this.$el).find("div.msg-emoji-class span.emoji-hover")).toggleClass("emoji-hover");
+            }
         }
     });
 });
